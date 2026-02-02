1391b2af0f429566ab7cebbb62fd66ea53ca9470
Nimeth Fernando <nimeth42@gmail.com>
2026-02-02 09:50:39 +0530

    Update project and ignore large models

--- basicsr/models/color_model.py
+++ basicsr/models/color_model.py
@@ -27,6 +27,16 @@ class ColorModel(BaseModel):
         # define network net_g
         self.net_g = build_network(opt['network_g'])
         self.net_g = self.model_to_device(self.net_g)
+        
+        # # PERFORMANCE OPTIMIZATION: torch.compile (requires PyTorch 2.0+)
+        # # This can provide a 20-30% speedup by fusing kernels
+        # if hasattr(torch, 'compile') and torch.cuda.is_available():
+        #     try:
+        #         self.net_g = torch.compile(self.net_g)
+        #         get_root_logger().info("Model successfully compiled with torch.compile for speed.")
+        #     except Exception as e:
+        #         get_root_logger().warning(f"torch.compile failed, proceeding with standard mode: {e}")
+
         self.print_network(self.net_g)
         
         # load pretrained model for net_g
@@ -38,6 +48,7 @@ class ColorModel(BaseModel):
         if self.is_train:
             self.init_training_settings()
             self.use_amp = self.opt['train'].get('use_amp', False)
+            # Mixed Precision setup
             self.scaler = torch.amp.GradScaler('cuda', enabled=self.use_amp)
 
     def init_training_settings(self):
@@ -47,28 +58,28 @@ class ColorModel(BaseModel):
         if self.ema_decay > 0:
             logger = get_root_logger()
             logger.info(f'Use Exponential Moving Average with decay: {self.ema_decay}')
-            # define network net_g with Exponential Moving Average (EMA)
-            # net_g_ema is used only for testing on one GPU and saving
-            # There is no need to wrap with DistributedDataParallel
             self.net_g_ema = build_network(self.opt['network_g']).to(self.device)
-            # load pretrained model
             load_path = self.opt['path'].get('pretrain_network_g', None)
             if load_path is not None:
                 self.load_network(self.net_g_ema, load_path, self.opt['path'].get('strict_load_g', True), 'params_ema')
             else:
-                self.model_ema(0)  # copy net_g weight
+                self.model_ema(0)
             self.net_g_ema.eval()
 
         # define network net_d
         self.net_d = build_network(self.opt['network_d'])
         self.net_d = self.model_to_device(self.net_d)
+        
+        # # Discriminators benefit from compile too
+        # if hasattr(torch, 'compile') and torch.cuda.is_available():
+        #     self.net_d = torch.compile(self.net_d)
+
         self.print_network(self.net_d)
 
-        # load pretrained model for net_d
         load_path = self.opt['path'].get('pretrain_network_d', None)
         if load_path is not None:
             param_key = self.opt['path'].get('param_key_d', 'params')
-            self.load_network(self.net_d, load_path, self.opt['path'].get('strict_load_d', True), param_key)
+            self.load_network(self.net_d, load_path, self.opt['path'].get('strict_load_g', True), param_key)
 
         self.net_g.train()
         self.net_d.train()
@@ -97,24 +108,15 @@ class ColorModel(BaseModel):
         else:
             self.cri_colorfulness = None
 
-        # set up optimizers and schedulers
         self.setup_optimizers()
         self.setup_schedulers()
 
-        # set real dataset cache for fid metric computing
-        self.real_mu, self.real_sigma = None, None
-        if self.opt['val'].get('metrics') is not None and self.opt['val']['metrics'].get('fid') is not None:
-            self._prepare_inception_model_fid()
+        # self.real_mu, self.real_sigma = None, None
+        # if self.opt['val'].get('metrics') is not None and self.opt['val']['metrics'].get('fid') is not None:
+        #     self._prepare_inception_model_fid()
 
     def setup_optimizers(self):
         train_opt = self.opt['train']
-        # optim_params_g = []
-        # for k, v in self.net_g.named_parameters():
-        #     if v.requires_grad:
-        #         optim_params_g.append(v)
-        #     else:
-        #         logger = get_root_logger()
-        #         logger.warning(f'Params {k} will not be optimized.')
         optim_params_g = self.net_g.parameters()
 
         # optimizer g
@@ -129,6 +131,7 @@ class ColorModel(BaseModel):
     
     def feed_data(self, data):
         self.lq = data['lq'].to(self.device)
+        # Using zero_like tensors for RGB conversion to avoid allocation overhead
         self.lq_rgb = tensor_lab2rgb(torch.cat([self.lq, torch.zeros_like(self.lq), torch.zeros_like(self.lq)], dim=1))
         if 'gt' in data:
             self.gt = data['gt'].to(self.device)
@@ -147,12 +150,9 @@ class ColorModel(BaseModel):
         
         with torch.amp.autocast('cuda', enabled=self.use_amp):
             self.output_ab = self.net_g(self.lq_rgb)
-        
-        # Color space conversion often needs float precision
-        self.output_lab = torch.cat([self.lq, self.output_ab], dim=1)
-        self.output_rgb = tensor_lab2rgb(self.output_lab)
+            self.output_lab = torch.cat([self.lq, self.output_ab], dim=1)
+            self.output_rgb = tensor_lab2rgb(self.output_lab)
 
-        with torch.amp.autocast('cuda', enabled=self.use_amp):
             l_g_total = 0
             loss_dict = OrderedDict()
             # pixel loss
@@ -208,171 +208,45 @@ class ColorModel(BaseModel):
         if self.ema_decay > 0:
             self.model_ema(decay=self.ema_decay)
 
-    def get_current_visuals(self):
-        out_dict = OrderedDict()
-        out_dict['lq'] = self.lq_rgb.detach().cpu()
-        out_dict['result'] = self.output_rgb.detach().cpu()
-        if self.opt['logger'].get('save_snapshot_verbose', False):  # only for verbose
-            self.output_lab_chroma = torch.cat([torch.ones_like(self.lq) * 50, self.output_ab], dim=1)
-            self.output_rgb_chroma = tensor_lab2rgb(self.output_lab_chroma)
-            out_dict['result_chroma'] = self.output_rgb_chroma.detach().cpu()
-
-        if hasattr(self, 'gt'):
-            out_dict['gt'] = self.gt_rgb.detach().cpu()
-            if self.opt['logger'].get('save_snapshot_verbose', False):  # only for verbose
-                self.gt_lab_chroma = torch.cat([torch.ones_like(self.lq) * 50, self.gt], dim=1)
-                self.gt_rgb_chroma = tensor_lab2rgb(self.gt_lab_chroma)
-                out_dict['gt_chroma'] = self.gt_rgb_chroma.detach().cpu()
-        return out_dict
-
     def test(self):
-        if hasattr(self, 'net_g_ema'):
-            self.net_g_ema.eval()
-            with torch.no_grad():
-                self.output_ab = self.net_g_ema(self.lq_rgb)
-                self.output_lab = torch.cat([self.lq, self.output_ab], dim=1)
-                self.output_rgb = tensor_lab2rgb(self.output_lab)
-        else:
-            self.net_g.eval()
-            with torch.no_grad():
-                self.output_ab = self.net_g(self.lq_rgb)
-                self.output_lab = torch.cat([self.lq, self.output_ab], dim=1)
-                self.output_rgb = tensor_lab2rgb(self.output_lab)
-            self.net_g.train()
-    
-    def dist_validation(self, dataloader, current_iter, tb_logger, save_img):
-        if self.opt['rank'] == 0:
-            self.nondist_validation(dataloader, current_iter, tb_logger, save_img)
+        self.net_g.eval()
+        with torch.no_grad():
+            self.output_ab = self.net_g(self.lq_rgb)
+            self.output_lab = torch.cat([self.lq, self.output_ab], dim=1)
+            self.output_rgb = tensor_lab2rgb(self.output_lab)
+        self.net_g.train()
     
     def nondist_validation(self, dataloader, current_iter, tb_logger, save_img):
         dataset_name = dataloader.dataset.opt['name']
         with_metrics = self.opt['val'].get('metrics') is not None
         use_pbar = self.opt['val'].get('pbar', False)
 
-        if with_metrics and not hasattr(self, 'metric_results'):  # only execute in the first run
+        if with_metrics and not hasattr(self, 'metric_results'):
             self.metric_results = {metric: 0 for metric in self.opt['val']['metrics'].keys()}
-        # initialize the best metric results for each dataset_name (supporting multiple validation datasets)
         if with_metrics:
             self._initialize_best_metric_results(dataset_name)
-        # zero self.metric_results
-        if with_metrics:
             self.metric_results = {metric: 0 for metric in self.metric_results}
 
         metric_data = dict()
-        if use_pbar:
-            pbar = tqdm(total=len(dataloader), unit='image')
+        pbar = tqdm(total=len(dataloader), unit='image') if use_pbar else None
         
         if self.opt['val']['metrics'].get('fid') is not None:
-            fake_acts_set, acts_set = [], []
+            fake_acts_set, acts
 
-        for idx, val_data in enumerate(dataloader):
-            # if idx == 100:
-            #     break
-            img_name = osp.splitext(osp.basename(val_data['lq_path'][0]))[0]
-            if hasattr(self, 'gt'):
-                del self.gt
-            self.feed_data(val_data)
-            self.test()
+def test(self):
+        self.net_g.eval()
+        with torch.no_grad():
+            self.output_ab = self.net_g(self.lq_rgb)
+            self.output_lab = torch.cat([self.lq, self.output_ab], dim=1)
+            self.output_rgb = tensor_lab2rgb(self.output_lab)
+        self.net_g.train()
 
-            visuals = self.get_current_visuals()
-            sr_img = tensor2img([visuals['result']])
-            metric_data['img'] = sr_img
-            if 'gt' in visuals:
-                gt_img = tensor2img([visuals['gt']])
-                metric_data['img2'] = gt_img
 
-            torch.cuda.empty_cache()
-
-            if save_img:
-                if self.opt['is_train']:
-                    save_dir = osp.join(self.opt['path']['visualization'], img_name)
-                    for key in visuals:
-                        save_path = os.path.join(save_dir, '{}_{}.png'.format(current_iter, key))
-                        img = tensor2img(visuals[key])
-                        imwrite(img, save_path)
-                else:
-                    if self.opt['val']['suffix']:
-                        save_img_path = osp.join(self.opt['path']['visualization'], dataset_name,
-                                                 f'{img_name}_{self.opt["val"]["suffix"]}.png')
-                    else:
-                        save_img_path = osp.join(self.opt['path']['visualization'], dataset_name,
-                                                 f'{img_name}_{self.opt["name"]}.png')
-                    imwrite(sr_img, save_img_path)
-
-            if with_metrics:
-                # calculate metrics
-                for name, opt_ in self.opt['val']['metrics'].items():
-                    if name == 'fid':
-                        pred, gt = visuals['result'].cuda(), visuals['gt'].cuda()
-                        fake_act = get_activations(pred, self.inception_model_fid, 1)
-                        fake_acts_set.append(fake_act)
-                        if self.real_mu is None:
-                            real_act = get_activations(gt, self.inception_model_fid, 1)
-                            acts_set.append(real_act)
-                    else:
-                        self.metric_results[name] += calculate_metric(metric_data, opt_)
-            if use_pbar:
-                pbar.update(1)
-                pbar.set_description(f'Test {img_name}')
-        if use_pbar:
-            pbar.close()
-
-        if with_metrics:
-            if self.opt['val']['metrics'].get('fid') is not None:
-                if self.real_mu is None:
-                    acts_set = np.concatenate(acts_set, 0)
-                    self.real_mu, self.real_sigma = calculate_activation_statistics(acts_set)
-                fake_acts_set = np.concatenate(fake_acts_set, 0)
-                fake_mu, fake_sigma = calculate_activation_statistics(fake_acts_set)
-
-                fid_score = calculate_frechet_distance(self.real_mu, self.real_sigma, fake_mu, fake_sigma)
-                self.metric_results['fid'] = fid_score
-
-            for metric in self.metric_results.keys():
-                if metric != 'fid':
-                    self.metric_results[metric] /= (idx + 1)
-                # update the best metric result
-                self._update_best_metric_result(dataset_name, metric, self.metric_results[metric], current_iter)
-
-            self._log_validation_metric_values(current_iter, dataset_name, tb_logger)
-
-    def _log_validation_metric_values(self, current_iter, dataset_name, tb_logger):
-        log_str = f'Validation {dataset_name}\n'
-        for metric, value in self.metric_results.items():
-            log_str += f'\t # {metric}: {value:.4f}'
-            if hasattr(self, 'best_metric_results'):
-                log_str += (f'\tBest: {self.best_metric_results[dataset_name][metric]["val"]:.4f} @ '
-                            f'{self.best_metric_results[dataset_name][metric]["iter"]} iter')
-            log_str += '\n'
-
-        logger = get_root_logger()
-        logger.info(log_str)
-        if tb_logger:
-            for metric, value in self.metric_results.items():
-                tb_logger.add_scalar(f'metrics/{dataset_name}/{metric}', value, current_iter)
-
-    def _prepare_inception_model_fid(self, path='pretrain/inception_v3_google-1a9a5a14.pth'):
-        incep_state_dict = torch.load(path, map_location='cpu')
-        block_idx = INCEPTION_V3_FID.BLOCK_INDEX_BY_DIM[2048]
-        self.inception_model_fid = INCEPTION_V3_FID(incep_state_dict, [block_idx])
-        self.inception_model_fid.cuda()
-        self.inception_model_fid.eval()
-
-    @master_only
-    def save_training_images(self, current_iter):
+def save_training_images(self, current_iter):
         visuals = self.get_current_visuals()
-        save_dir = osp.join(self.opt['root_path'], 'experiments', self.opt['name'], 'training_images_snapshot')
-        os.makedirs(save_dir, exist_ok=True)
-
-        for key in visuals:
-            save_path = os.path.join(save_dir, '{}_{}.png'.format(current_iter, key))
-            img = tensor2img(visuals[key])
-            imwrite(img, save_path)
-
-    def save(self, epoch, current_iter):
-        if hasattr(self, 'net_g_ema'):
-            self.save_network([self.net_g, self.net_g_ema], 'net_g', current_iter, param_key=['params', 'params_ema'])
-        else:
-            self.save_network(self.net_g, 'net_g', current_iter)
-        self.save_network(self.net_d, 'net_d', current_iter)
-        self.save_training_state(epoch, current_iter)
+        save_img_path = osp.join(self.opt['path']['visualization'], f'iter_{current_iter}.png')
+        img_lq = tensor2img(self.lq_rgb[0]) 
+        img_gt = tensor2img(visuals['gt_rgb'][0])
+        img_res = tensor2img(visuals['result'][0])
+        img_concat = np.concatenate((img_lq, img_gt, img_res), axis=1)
+        imwrite(img_concat, save_img_path)
